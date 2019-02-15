0604dd077072ad056a0bd076ebad6893e918e9d7
Michael Jordan <mkerememj10@gmail.com>
2019-02-15 11:43:55 +0900

    Add files via upload

---
+++ anagram2.py
@@ -0,0 +1,28 @@
+# Anagram2
+
+def character_count(string):
+    characters = {}
+    for ch in string:
+        if ch in characters:
+            characters[ch] = character[ch]+1
+        else:
+            characters[ch] = 1
+    return characters
+
+def main():
+    word1 = input("Enter first word: ")
+    word2 = input("Enter second word: ")
+
+    count1 = character_count(word1)
+    count2 = character_count(word2)
+    if count1 ==count2:
+        print("Those strings are anagrams")
+    else:
+        print("Those strings are not anagrams")
+
+if __name__=="__main__":
+    main()
+
+    
+
+        
